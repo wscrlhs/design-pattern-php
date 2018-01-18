094e0fd00628430aa0a3b4da46882921ab633a99
wscrlhs <wscrlhs@gmail.com>
2018-01-18 20:21:24 +0800

    原型模式

---
+++ PrototypePattern/PrototypePattern.php
@@ -0,0 +1,34 @@
+<?php
+
+interface Prototype
+{
+    public function copy();
+}
+
+
+class ConcretePrototype implements Prototype
+{
+    private $_name;
+
+    public function _construct($name)
+    {
+        $this->_name = $name;
+    }
+
+    public function copy()
+    {
+        // TODO: Implement copy() method.
+        return clone $this;
+    }
+}
+
+class Demo
+{
+
+}
+
+
+$demo = new Demo();
+$object1 = new ConcretePrototype($demo);
+$object2 = $object1->copy();
+
